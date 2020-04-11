bdd2bc47d4575b47be60936aaaa0edbdebd60e61
Hogeun Yoo <hogeunyoo@korea.ac.kr>
2020-04-11 05:04:04 +0100

    REFACTOR : 코드 사용 편의성 향상 및 중복 코드 제거

--- vilageFcstInfoService.py
+++ vilageFcstInfoService.py
@@ -1,5 +1,5 @@
 import serviceKey
-import json, datetime
+import json, datetime,pytz
 
 from customType import CodeValue, VilageFcstInfoService
 from urllib.request import urlopen, Request
@@ -17,8 +17,8 @@ class RequestParameter:
         self.ny = ny                  # 121
 
 class VilageFcstInfoServiceDatas(RequestParameter):
-    def __init__(self,serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI,codeValue):
-        def request():
+    def __init__(self,serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI):
+        def request(getAPI):
             url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/' + getAPI.value 
             queryParams = '?' + 'serviceKey=' + serviceKey  + '&' + urlencode({ 
                     quote_plus('pageNo') : pageNo,
@@ -45,11 +45,8 @@ class VilageFcstInfoServiceDatas(RequestParameter):
             return status
 
         super().__init__(serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny)
-        self.__getAPI: VilageFcstInfoService = getAPI
-        self.__codeValue: CodeValue = codeValue
-        self.json_datas: dict = json.loads(request())
+        self.json_datas: dict = json.loads(request(getAPI))
         self.is_normal_service = get_service_status()
-
 
     def get_error_message(self):
         if self.is_normal_service is not None:
@@ -58,7 +55,9 @@ class VilageFcstInfoServiceDatas(RequestParameter):
             print('알 수 없는 에러__001')
 
 class VilageFcstInfoServices(VilageFcstInfoServiceDatas):
-    def __init__(self,serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI,codeValue):
+    __today = datetime.datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime('%Y%m%d')
+    __now = datetime.datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime('%H%M')
+    def __init__(self,serviceKey,nx,ny,getAPI,codeValue,pageNo=1,numOfRows=10,dataType='JSON',base_date=__today,base_time=__now):
         def get_std_date(date_now: str = base_date, time_now : str = base_time) : # 표준시간 생성함수
                 standard_time = [2, 5, 8, 11, 14, 17, 20, 23]
                 check_time = (int(time_now) // 100)
@@ -73,7 +72,6 @@ class VilageFcstInfoServices(VilageFcstInfoServiceDatas):
                 
                 print(check_date)
                 return (str(check_date), check_time) #자릿수 문제
-
         def response_api(json_datas):
             response_data = None
             if self.is_normal_service == True:
@@ -93,25 +91,18 @@ class VilageFcstInfoServices(VilageFcstInfoServiceDatas):
                 print('Error : 알 수 없는 에러_002')
             return response_data
         if getAPI == VilageFcstInfoService.GetUltraSrtFcst or getAPI == VilageFcstInfoService.GetUltraSrtNcst:
-            super().__init__(serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI,codeValue)
+            super().__init__(serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI)
         elif getAPI == VilageFcstInfoService.GetVilageFcst :
-            std_date, std_time = get_std_date(date_now= base_date, time_now= base_time)
+            std_date, std_time = get_std_date(base_date, base_time)
             print('표준변환:', std_date, std_time)
-            super().__init__(serviceKey,pageNo,numOfRows,dataType,std_date,std_time,nx,ny,getAPI,codeValue)
+            super().__init__(serviceKey,pageNo,numOfRows,dataType,std_date,std_time,nx,ny,getAPI)
         else:
             print('Error : getAPI 값 오류')
-            super().__init__(serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI,codeValue)
+            super().__init__(serviceKey,pageNo,numOfRows,dataType,base_date,base_time,nx,ny,getAPI)
         self.result = response_api(self.json_datas)
-            
+        
 
         
-            
-
-
 # 작동 확인 테스트 코드
-# import pytz
-# today = datetime.datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime('%Y%m%d')
-# now = datetime.datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime('%H%M')
-
-# result = VilageFcstInfoServices(serviceKey=serviceKey.key,pageNo=1,numOfRows=10,dataType='Json',base_date=today,base_time=now,nx=61,ny=127,getAPI=VilageFcstInfoService.GetVilageFcst,codeValue=CodeValue.SKY).result
-# print(result)
+result = VilageFcstInfoServices(serviceKey=serviceKey.key,nx=61,ny=127,getAPI=VilageFcstInfoService.GetVilageFcst,codeValue=CodeValue.SKY).result
+print(result)
